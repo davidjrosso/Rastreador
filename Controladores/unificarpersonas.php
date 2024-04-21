67111c1a7415e9f81ce27cf054f87da00cec04fa
MartinMonnittola <martinmonnittola@hotmail.com>
2024-04-20 22:33:15 -0300

    Se corrigio un error en la redireccion de la aplicacion luego de unificar dos personas.

--- Controladores/unificarpersonas.php
+++ Controladores/unificarpersonas.php
@@ -19,7 +19,6 @@ require_once 'Conexion.php';
  * along with Rastreador3; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
-
 $ID_Solicitud = isset($_REQUEST["ID_Solicitud"])? $_REQUEST["ID_Solicitud"] : null;
 $ID_Persona_1 = $_REQUEST["ID_Persona_1"];
 $ID_Persona_2 = $_REQUEST["ID_Persona_2"];
@@ -27,23 +26,30 @@ $ID_Usuario = isset($_SESSION["Usuario"])? $_SESSION["Usuario"]: null;
 if($ID_Persona_1 > 0 && $ID_Persona_2 > 0){
 	$Con = new Conexion();
 	$Con->OpenConexion();
-
+	
 	if(($ID_Solicitud != null && $ID_Solicitud != "" )){
-		$Consulta = "update movimiento set id_persona = $ID_Persona_1 where id_persona = $ID_Persona_2";
-
+		
+		$Consulta = "update movimiento 
+		set id_persona = $ID_Persona_1 
+		where id_persona = $ID_Persona_2";
+		
 		mysqli_query($Con->Conexion,$Consulta)or die("Problemas en la consulta");
+		
+		$ConsultaBajaPersona = "update persona 
+								set estado = 0 
+								where id_persona = $ID_Persona_2";
 
-
-		$ConsultaBajaPersona = "update persona set estado = 0 where id_persona = $ID_Persona_2";
 		$MensajeErrorBajaPersona = "No se pudo dar de baja la persona";
 
 		mysqli_query($Con->Conexion,$ConsultaBajaPersona) or die($MensajeErrorBajaPersona);
 
-		$ConsultaSolicitud = "update solicitudes_unificacion set Estado = 0 where ID_Solicitud_Unificacion = $ID_Solicitud";
+		$ConsultaSolicitud = "update solicitudes_unificacion
+							  set Estado = 0 
+							  where ID_Solicitud_Unificacion = $ID_Solicitud";
+
 		if(!$Ret = mysqli_query($Con->Conexion,$ConsultaSolicitud)){
 			throw new Exception("Problemas en la consulta. Consulta: ".$ConsultaSolicitud, 3);			
 		}
-
 		$Con->CloseConexion();
 		$Mensaje = "Los datos se unificaron Correctamente";
 		header('Location: ../view_inicio.php?Mensaje='.$Mensaje);
@@ -51,16 +57,17 @@ if($ID_Persona_1 > 0 && $ID_Persona_2 > 0){
 		$Consulta = "update movimiento set id_persona = $ID_Persona_1 where id_persona = $ID_Persona_2";
 		mysqli_query($Con->Conexion,$Consulta)or die("Problemas en la consulta");
 
-		$ConsultaBajaPersona = "update persona set estado = 0 where id_persona = $ID_Persona_2";
-		$MensajeErrorBajaPersona = "No se pudo dar de baja la persona";
+		$ConsultaBajaPersona = "update persona 
+								set estado = 0 
+								where id_persona = $ID_Persona_2";
 
+		$MensajeErrorBajaPersona = "No se pudo dar de baja la persona";
 		mysqli_query($Con->Conexion,$ConsultaBajaPersona) or die($MensajeErrorBajaPersona);
+		$Mensaje = "Los datos se unificaron Correctamente";
+		header('Location: ../view_inicio.php?Mensaje='.$Mensaje);
 	}
 }else{
 	$MensajeError = "Debe seleccionar Primera Persona y Segunda Persona";
 	header('Location: ../view_inicio.php?MensajeError='.$MensajeError);
 }
-
-
-
 ?>
