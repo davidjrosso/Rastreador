e24251dd681b44f876e3d21e044db76aa7e75cb1
MartinMonnittola <martinmonnittola@hotmail.com>
2024-07-02 09:53:40 -0300

    Modificacion ortografica de los botones en la creacion de nueva categoria

--- view_newcategorias.php
+++ view_newcategorias.php
@@ -276,7 +276,7 @@ $Con->CloseConexion();
             <div class="form-group row">
               <div class="offset-md-2 col-md-10">
                 <button type="button" class="btn btn-outline-success" onClick ="Verificar();">Guardar</button>
-                <button type = "button" class = "btn btn-danger" onClick = "location.href = 'view_categorias.php'">Atras</button>
+                <button type = "button" class = "btn btn-danger" onClick = "location.href = 'view_categorias.php'">Atrás</button>
               </div>
             </div>
           </form>
