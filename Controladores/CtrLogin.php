3789baf899c6a50d9feff1ea7442abacd83d1c5f
MartinMonnittola <martinmonnittola@hotmail.com>
2024-07-04 19:37:22 -0300

    Correccion en los meses asociado a una persona mostrado en el reporte por listado

--- Controladores/CtrLogin.php
+++ Controladores/CtrLogin.php
@@ -22,7 +22,7 @@ while($RetDatosPersonas = mysqli_fetch_assoc($EjecutarConsultarDatosPersonas)){
 		$ano_diferencia = date("Y") - $ano;
 		$mes_diferencia = date("m") - $mes;
 		$dia_diferencia = date("d") - $dia;
-		if($dia_diferencia < 0 || $mes_diferencia < 0){
+		if($dia_diferencia < 0 || $mes_diferencia > 0){
 			$ano_diferencia--;
 		}
 		$Edad = $ano_diferencia;
