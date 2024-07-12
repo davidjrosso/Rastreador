4bb35df0db96d4650a14456d523e09e378b6cf2a
MartinMonnittola <martinmonnittola@hotmail.com>
2024-07-12 17:52:18 -0300

    Correccion en la sintaxis de consulta de movimiento en la pagina de reporte grafico

--- view_rep_general_new.php
+++ view_rep_general_new.php
@@ -1797,7 +1797,7 @@ $Con->CloseConexion();
                                                         and (motivo_1 <> 1
                                                          or motivo_2 <> 1
                                                          or motivo_3 <> 1) 
-                                                        and estado = 1;
+                                                        and estado = 1
                                                       order by fecha";
                     
                     // echo "<br> DEBUG CONSULTAR MOVIMIENTO: ".var_dump($Consultar_Movimientos_Persona);
