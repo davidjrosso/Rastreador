304587166fa4725875d4855aa09d65b07f4fdd02
MartinMonnittola <martinmonnittola@hotmail.com>
2024-08-01 19:33:03 -0300

    Correccion de caracteres en el formulario de creacion de categoria

--- view_newcategorias.php
+++ view_newcategorias.php
@@ -252,27 +252,27 @@ $Con->CloseConexion();
           <!-- Carga -->
           <p class = "Titulos">Cargar Nueva Categoría</p>
           <form method = "post" onKeydown="return event.key != 'Enter';" id = "form_1" action = "Controladores/CrearCategoria.php" onSubmit = "return ValidarCategoria();">
-            <div class="form-group row">
-              <label for="inputPassword" class="col-md-2 col-form-label LblForm">Código : </label>
+          <div class="form-group row">
+              <label id="denominacion" for="Categoria" class="col-md-2 col-form-label LblForm">Denominación</label>
               <div class="col-md-10">
-                <input type="text" class="form-control" name = "Codigo" id="Codigo" autocomplete="off">
+                <input type="text" class="form-control" name = "Categoria" id="Categoria" autocomplete="off">
               </div>
             </div>
             <div class="form-group row">
-              <label for="inputPassword" class="col-md-2 col-form-label LblForm">Forma : </label>
+              <label for="inputPassword" class="col-md-2 col-form-label LblForm">Forma</label>
               <div class="col-md-10">
                 <?php $Element = new Elements();
                 echo $Element->CBFormas_Categoria();?>
               </div>
             </div>
             <div class="form-group row">
-              <label id="denominacion" for="Categoria" class="col-md-2 col-form-label LblForm">Denominación : </label>
+              <label for="inputPassword" class="col-md-2 col-form-label LblForm">Código</label>
               <div class="col-md-10">
-                <input type="text" class="form-control" name = "Categoria" id="Categoria" autocomplete="off">
+                <input type="text" class="form-control" name = "Codigo" id="Codigo" autocomplete="off">
               </div>
             </div>
             <div class="form-group row">
-              <label id="grupousuarios" for="grupousuarios" class="col-md-2 col-form-label LblForm">Permisos : </label>
+              <label id="grupousuarios" for="grupousuarios" class="col-md-2 col-form-label LblForm">Permisos</label>
               
               <div class="col-md-10">
                 <?php $Element = new Elements();
