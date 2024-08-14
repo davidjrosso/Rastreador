09af1752e13e1dee4ddc1572f2b5adb71aff236f
MartinMonnittola <martinmonnittola@hotmail.com>
2024-08-14 18:17:03 -0300

    Correccion del conteo de numero de filas a mostrar, se descartan filas extras

--- Controladores/GeneradorPdf.php
+++ Controladores/GeneradorPdf.php
@@ -220,7 +220,7 @@ try{
             </html>";
     } elseif ($from_reporte_listado) {
         $count = count($array_filas);
-        $count += (isset($array_filas["det_persona"]) ? 2 : 0);
+        $count -= (isset($array_filas["det_persona"]) ? 5 : 3);
         for ($i = 0; $i < (count($array_filas) - 5); $i++) {
             $row .= "<tr>";
             for ($h = 0; $h < count($header_mov_general); $h++) {
