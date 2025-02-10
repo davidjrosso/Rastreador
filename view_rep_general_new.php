84881f3a2736314e900ac1c965a52cc479b420a4
MartinMonnittola <martinmonnittola@hotmail.com>
2025-02-10 08:17:14 -0300

    elementos elegidos en el reporte por listado

--- view_rep_general_new.php
+++ view_rep_general_new.php
@@ -187,6 +187,10 @@ $Con->CloseConexion();
         columnaRemoverClass.removeClass("showColTablaAnimacionfire");
         columnaRemoverClass.removeClass("showColTablaAnimacion");
       });
+
+      $("#boton-desplegale").on("click", function (e) {
+        $("#desplegable").toggle();
+      });
       /*
       $("#map-modal").on("transitionend", function (e) {
         if (!map) {
@@ -2915,19 +2919,21 @@ $Con->CloseConexion();
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
+          <button type="button" id="boton-desplegale" class="buttom-arrow" aria-label="desplegable">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
+          </svg>
+          </button>
         </div>
         <div class="modal-body" style="padding-top: 0px">
           <div id="basicMap"></div>
-
-          <!--<div style="pointer-events: none; position: absolute; top: 30px; left: 70px; z-index: 1000">
+          <div id="desplegable" style="display: none; pointer-events: none; position: absolute; top: 3px; left: 20px; z-index: 1000">
             <?php
-            /*
             foreach ($filtros as $value) {
                 echo "<span class='etFiltros'>" . $value . "</span> ";
             }
-            */
             ?>
-          </div>-->
+          </div>
         </div>
       </div>
     </div>
