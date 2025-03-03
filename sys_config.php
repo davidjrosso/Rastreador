38a7911153e6fdc44d26e0da8cddd55aac0c19d5
MartinMonnittola <martinmonnittola@hotmail.com>
2025-03-03 16:40:14 -0300

    Configuracion de conexion a cuenta drive

--- sys_config.php
+++ sys_config.php
@@ -5,4 +5,3 @@ define('TYPE_ACCOUNT', 'service_account');
 define('PROJECT_ID', 'vast-collective-448820-m1');
 define('CLIENT_ID', '116955062035755741051');
 define('CLIENT_EMAIL', 'desarrollo@vast-collective-448820-m1.iam.gserviceaccount.com');
-define('FILE_ID', '1gQkq17UmP0GaYGY6Xt8c7eIe52jk0ZG7TMxdfb6wM1M');
