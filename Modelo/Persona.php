1d8b7a1d62a7c93d4616990fa51ecf68d596d2c2
MartinMonnittola <martinmonnittola@hotmail.com>
2025-04-17 15:01:40 -0300

    Correccion de consulta de personas

--- Modelo/Persona.php
+++ Modelo/Persona.php
@@ -1061,7 +1061,7 @@ public function update_direccion()
 					 calle = " . ((!is_null($this->getId_Calle())) ? $this->getId_Calle() : "null") . ", 
 					 nro = " . ((!is_null($this->getNro())) ? $this->getNro() : "null") . ", 
 					 familia = " . ((!is_null($this->getFamilia())) ? $this->getFamilia() : "null") . ",
-					 ID_Barrio = " . ((!is_null($this->getId_Barrio())) ? $this->getId_Barrio() : "null") . ",  
+					 ID_Barrio = " . ((!is_null($this->getId_Barrio())) ? $this->getId_Barrio() : "null") . "
 				 where id_persona = " . $this->getID_Persona();
 				 $mensaje_error_consulta = "No se pudo actualizar la Persona";
 				 if (!$Ret = mysqli_query($Con->Conexion, $Consulta)) {
