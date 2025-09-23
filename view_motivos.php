fe5fae5144a339ecbf0f12375a303193e99c3ffd
MartinMonnittola <martinmonnittola@hotmail.com>
2025-09-22 23:30:13 -0300

    Modifiacion de estilo de paginas de motivos.

--- view_motivos.php
+++ view_motivos.php
@@ -21,6 +21,7 @@ $TipoUsuario = $account->get_id_tipo_usuario();
   <title>Rastreador III</title>
   <meta charset="utf-8">
   <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
   <link rel="stylesheet" type="text/css" href="css/Estilos.css">
   <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
@@ -62,12 +63,12 @@ $TipoUsuario = $account->get_id_tipo_usuario();
 
 </head>
 <body>
-<div class = "row">
+<div class = "row margin-right-cero">
 <?php
   $Element = new Elements();
   echo $Element->menuDeNavegacion($TipoUsuario, $ID_Usuario, $Element::PAGINA_MOTIVO);
   ?>
-  <div class = "col-md-9">
+  <div class = "col-md-9 inicio-md-2">
     <div class="row">
       <div class="col"></div>
       <div class="col-10 Titulo">
