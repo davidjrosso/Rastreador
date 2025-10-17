5b5f3aee0b483d8dc04f8bf6839eef6a87ffeaa9
MartinMaximilianoMonnittola <system.bridge.tec@gmail.com>
2025-10-17 19:37:09 -0300

    Correccion en la modificacion de calle de una persona

--- Controladores/ModificarPersona.php
+++ Controladores/ModificarPersona.php
@@ -200,12 +200,6 @@ try {
 		$Persona_Viejo->setNro($Persona->getNro());
 		$Persona_Viejo->setCalle($Persona->getId_Calle());
 
-		if ($Persona->getId_Calle() && $Persona->getNro()){
-			$Persona_Viejo->setDomicilio();	
-		} else {
-			$Persona_Viejo->setDomicilio($Persona->getDomicilio());
-		}
-
 		if ($georeferencia_point) {
 			$Persona_Viejo->setGeoreferencia(xGeoreferencia: $georeferencia_point);
 		}
