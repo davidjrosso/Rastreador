f68952cd6164780550b827ccfe471844a319804f
MartinMaximilianoMonnittola <system.bridge.tec@gmail.com>
2025-10-17 17:04:05 -0300

    Funcionalidad de accion de insercion de categoria en controlador de categoria.

--- Controladores/CategoriaController.php
+++ Controladores/CategoriaController.php
@@ -193,6 +193,124 @@ class CategoriaController
         exit();  
     }
 
+    public function insertar_categoria_control()
+    {
+        header('Content-Type: application/json;');
+        $ID_Usuario = $_SESSION["Usuario"];
+        $ID_Solicitud = $_REQUEST["ID"];
+        $Codigo = strtoupper($_REQUEST["Codigo"]);
+        $ID_Forma = $_REQUEST["ID_Forma"];
+        $Categoria = $_REQUEST["Categoria"];
+        $Color = base64_decode($_REQUEST["Color"]);
+        $Fecha = date("Y-m-d");
+        $ID_TipoAccion = 1;
+        $Detalles = "El usuario con ID: $ID_Usuario ha registrado una nueva Categoria. Datos: $Codigo - $Categoria";
+
+        try {
+            $Con = new Conexion();
+            $Con->OpenConexion();
+
+            $ConsultarRegistrosIguales = "select * from categoria where cod_categoria = '$Codigo' and estado = 1";
+            if(!$RetIguales = mysqli_query($Con->Conexion,$ConsultarRegistrosIguales)){
+                throw new Exception("Problemas al consultar registros iguales. Consulta: ".$ConsultarRegistrosIguales, 0);		
+            }
+            $Resultado = mysqli_num_rows($RetIguales);
+            if ($Resultado > 0) {
+                mysqli_free_result($RetIguales);
+                $Con->CloseConexion();
+                $Mensaje = "Ya existe una categoria con ese Codigo";
+                $response["mensaje_error"] = $Mensaje;
+                echo json_encode($response);
+            } else {
+                $Consulta = "insert into categoria(cod_categoria,categoria,ID_Forma,color,estado) values('".$Codigo."','".$Categoria."',".$ID_Forma.",'".$Color."',1)";
+                if(!$Ret = mysqli_query($Con->Conexion,$Consulta)){
+                    throw new Exception("Problemas en la consulta. Consulta: ".$Consulta, 1);		
+                }
+
+                $ConsultarID_Categoria = "select id_categoria from categoria where cod_categoria = '$Codigo' and categoria = '$Categoria' limit 1";
+                if (!$RetID = mysqli_query($Con->Conexion,$ConsultarID_Categoria)) {
+                    throw new Exception("No se pudo consultar el ID de la categoria cargada. Consulta: ".$ConsultarID_Categoria, 2);		
+                }
+
+                $TomarID_Categoria = mysqli_fetch_assoc($RetID);
+                $RetID_Categoria = $TomarID_Categoria["id_categoria"];
+
+                $ConsultaPermisos = "select ID_TipoUsuario
+                                    from solicitudes_permisos
+                                    where ID = {$ID_Solicitud}
+                                    and estado = 1";
+                $MessageError = "Problemas al consultar mostrar Solicitudes Permisos";
+                if (!$Resultados = mysqli_query($Con->Conexion,$ConsultaPermisos)) {
+                    throw new Exception("No se pudo insertar el conjunto de permisos. Consulta: ".$ConsultaPermisos, 2);
+                }
+
+                while ($RetPermisos = mysqli_fetch_array($Resultados)) {
+                    $GrupoUsuarios = $RetPermisos["ID_TipoUsuario"];
+                    $Insert_Permiso = "insert into categorias_roles(id_categoria, fecha, ID_TipoUsuario, estado) values('{$RetID_Categoria }', '{$Fecha}','{$GrupoUsuarios}', 1)";
+                    $updatePermisos = "update solicitudes_permisos
+                                    set estado = 0
+                                    where ID = {$ID_Solicitud}
+                                    and ID_TipoUsuario = {$GrupoUsuarios} 
+                                    and estado = 1";
+                    $MensajeError = "No se pudo dar de baja el permiso categoria {$RetID_Categoria } rol {$GrupoUsuarios}";
+                    $ResultadosUpdate = mysqli_query($Con->Conexion,$updatePermisos) or die($MessageError);
+                    if(!$RetID = mysqli_query($Con->Conexion,$Insert_Permiso)){
+                        throw new Exception("No se pudo actualizar el permisos. Consulta: ".$Insert_Permiso, 2);
+                    }
+                }
+
+                $Consulta = "select * 
+                            from formas_categorias 
+                            where id_forma = $ID_Forma";
+                $consulta_resultado = mysqli_query($Con->Conexion,$Consulta);
+                if (!$consulta_resultado) {
+                    throw new Exception("Problemas en la consulta. Consulta: ".$Consulta, 1);		
+                }
+                $forma_categoria_row = mysqli_fetch_assoc($consulta_resultado);
+
+                $forma_categoria = $forma_categoria_row["Forma_Categoria"];
+                if (strlen($forma_categoria) > 1) {
+                    $forma_categoria = substr($forma_categoria, 2);
+                    $forma_categoria = substr($forma_categoria, 0, -1);
+                } elseif (strlen($forma_categoria) == 1) {
+                    $forma_categoria = ord($forma_categoria);
+                }
+                list($r, $g, $b) = sscanf($Color, "#%02x%02x%02x");
+                $color_icono = substr($Color, 1);
+                $file_path = "../images/icons/motivos/" . $forma_categoria . "_" . $color_icono . ".png";
+                $file_path_common = "../images/icons/motivos/" . $forma_categoria . ".png";
+
+                if (!file_exists($file_path)) {
+                    $imagen = imagecreatefrompng($file_path_common);
+                    $is_filter = imagefilter($imagen, IMG_FILTER_COLORIZE, $r, $g, $b);
+                    $negro = imagecolorallocate($imagen, 0, 0, 0);
+                    imagecolortransparent($imagen, $negro);
+                    if (!$is_filter) {
+                        throw new Exception("Error al intentar filtrar el color del icono.", 2);
+                    }
+                    $fd = imagepng($imagen, $file_path);
+                    imagedestroy($imagen);
+                }
+
+                $ConsultaAccion = "insert into Acciones(accountid,Fecha,Detalles,ID_TipoAccion) values($ID_Usuario,'$Fecha','$Detalles',$ID_TipoAccion)";
+                if (!$RetAccion = mysqli_query($Con->Conexion,$ConsultaAccion)) {
+                    throw new Exception("Error al intentar registrar Accion. Consulta: ".$ConsultaAccion, 2);
+                }
+
+                $MensajeErrorDatos = "Error al actualizar las solicitudes de categoria.";
+                $ActualizarSolicitud = "update solicitudes_crearcategorias set Estado = 0 where codigo = '$Codigo' and categoria = '$Categoria'";
+                $EjecutarConsultar = mysqli_query($Con->Conexion,$ActualizarSolicitud) or die($MensajeErrorDatos);
+
+                $Con->CloseConexion();
+                $Mensaje = "La categoria se creo Correctamente";
+                $response["mensaje"] = $Mensaje;
+                echo json_encode($response);
+            }
+        } catch (Exception $e) {
+            echo "Error: ".$e->getMessage();
+        }
+    }
+
     public function sol_del_control($id_categoria)
     {
         $ID_Usuario = $_SESSION["Usuario"];
