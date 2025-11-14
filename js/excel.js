ab419e89a09b92af5ae7aa50413d0c1d6ffd39c1
Monnittola <system.bridge.tec@gmail.com>
2025-11-14 15:39:18 -0300

    Modificacion de tolbars de excel de pagina de listado de movimientos

--- js/excel.js
+++ js/excel.js
@@ -56,74 +56,11 @@ export function excel() {
                 data: elems,
                 columns: list,
                 filters: true,
-                allowComments:true
+                allowComments:true,
+                search: true
                 }],
-                toolbar:[{
-                        type: 'i',
-                        content: 'undo',
-                        onclick: function() {
-                            table.undo();
-                        }
-                    },
-                    {
-                        type: 'i',
-                        content: 'redo',
-                        onclick: function() {
-                            table.redo();
-                        }
-                    },
-                    {
-                        type: 'i',
-                        content: 'save',
-                        onclick: function () {
-                            table.download();
-                        }
-                    },
-                    {
-                        type: 'select',
-                        k: 'font-family',
-                        v: ['Arial','Verdana']
-                    },
-                    {
-                        type: 'select',
-                        k: 'font-size',
-                        v: ['9px','10px','11px','12px','13px','14px','15px','16px','17px','18px','19px','20px']
-                    },
-                    {
-                        type: 'i',
-                        content: 'format_align_left',
-                        k: 'text-align',
-                        v: 'left'
-                    },
-                    {
-                        type:'i',
-                        content:'format_align_center',
-                        k:'text-align',
-                        v:'center'
-                    },
-                    {
-                        type: 'i',
-                        content: 'format_align_right', 
-                        k: 'text-align',
-                        v: 'right'
-                    },
-                    {
-                        type: 'i',
-                        content: 'format_bold',
-                        k: 'font-weight',
-                        v: 'bold'
-                    },
-                    {
-                        type: 'color',
-                        content: 'format_color_text',
-                        k: 'color'
-                    },
-                    {
-                        type: 'color',
-                        content: 'format_color_fill',
-                        k: 'background-color'
-                    },
-                ]
+                search: true,
+                toolbar:true
         });
     }
     
