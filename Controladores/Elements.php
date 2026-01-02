69bf1c3dddc757172a97da557e416d5ea5ea5f9e
Monnittola <system.bridge.tec@gmail.com>
2026-01-02 10:45:34 -0300

    Inicializacion de excel de 2026 en listado de planillas

--- Controladores/Elements.php
+++ Controladores/Elements.php
@@ -2820,7 +2820,8 @@ public function getMenuSeguridadUsuario($ID){
         $consulta = "SELECT * 
                      FROM archivos
                      WHERE centro_salud = " . $Ret['id_centro'] . "
-                       AND estado = 1";
+                       AND estado = 1
+                     ORDER BY planilla RLIKE '2026' DESC";
         $result = mysqli_query($con->Conexion,$consulta) or die("Problemas al mostrar los archivos");
         while ($row = mysqli_fetch_array($result)) {
             $div .= "<tr>
