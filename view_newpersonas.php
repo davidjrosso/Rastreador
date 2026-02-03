41ea10d5df0a3f0f6f242f519b06913725d33176
Monnittola <system.bridge.tec@gmail.com>
2026-02-03 16:14:05 -0300

    Info de ley pacientes.

--- view_newpersonas.php
+++ view_newpersonas.php
@@ -406,7 +406,7 @@
                 </div>
                 <!-- TOAST PROGRESO ENLACE -->
                 <span id="liveToast" class="position-relative top-0 end-0 p-3 toast hide dat-toast" style="border-radius: 10%;top: -40%;">
-                    Progeso
+                    Ley N° 26.743 - Decreto 476/202
                 </span>
                 <!-- FIN TOAST PROGRESO ENLACE -->
               </div>
