fb72b5cdf6993ca91932aa7e2995f4ae73cbc866
Monnittola <system.bridge.tec@gmail.com>
2026-02-23 00:41:28 -0300

    Correccion de datos en reporte por listado

--- view_vermovlistados.php
+++ view_vermovlistados.php
@@ -556,7 +556,7 @@ if (empty($_REQUEST["ID_Persona"])) {
               $ID_CentroSalud = $_REQUEST["ID_CentroSalud"];
               $ID_OtraInstitucion = $_REQUEST["ID_OtraInstitucion"];
               $ID_Responsable = array_filter($_REQUEST["ID_Responsable"], function ($e, $val) {
-                return !empty($val);
+                return !empty($e);
               }, ARRAY_FILTER_USE_BOTH);
 
               $motivos = array_filter($MotivosOpciones, 
