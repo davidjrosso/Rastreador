0a3525f16108a6e9c766c7a1bbee64b254802d85
Monnittola <system.bridge.tec@gmail.com>
2026-03-05 16:42:31 -0300

    Correccion de require de modulo de mod motivo

--- Controladores/ModificarMotivo.php
+++ Controladores/ModificarMotivo.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 require_once 'Conexion.php';
-require_once($_SERVER["DOCUMENT_ROOT"] . "\Modelo\Solicitud_ModificarMotivo.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . "/Modelo/Solicitud_ModificarMotivo.php");
 
 /*
  *
