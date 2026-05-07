ba8f19555a1ec9edc2407625f255f6cca5953ef1
Monnittola <system.bridge.tec@gmail.com>
2026-05-06 23:05:23 -0300

    ruter index ls

--- Controladores/CalleController.php
+++ Controladores/CalleController.php
@@ -567,4 +567,41 @@ class CalleController
             echo "Error: " . $e->getMessage();
         }        
     }
+
+    public function listar_calles()
+    {
+        header('Content-Type: application/json;');
+
+        $consultaBusqueda = (!isset($_REQUEST['calle']) ? null : $_REQUEST['calle']);
+
+        //Filtro anti-XSS
+        $caracteres_malos = array("<", ">", "\"", "'", "/", "<", ">", "'", "/");
+        $caracteres_buenos = array("& lt;", "& gt;", "& quot;", "& #x27;", "& #x2F;", "& #060;", "& #062;", "& #039;", "& #047;");
+        $consultaBusqueda = str_replace($caracteres_malos, $caracteres_buenos, $consultaBusqueda);
+        $consultaBusqueda = strtolower($consultaBusqueda);
+        $resultados = [];
+
+        if (!is_null($consultaBusqueda)) {
+
+            $con = new Conexion();
+            $con->OpenConexion();
+            $consultaBusqueda = trim(strtolower($consultaBusqueda));
+            $consultaCalles = "SELECT id_calle, calle_nombre
+                            FROM calles
+                            WHERE estado = 1
+                                AND ((LOWER(calle_nombre) REGEXP '[a-z]* " . $consultaBusqueda . "[a-z]*')
+                                    or (LOWER(calle_nombre) REGEXP '^" .  $consultaBusqueda . "[a-z]*'))
+                            order by calle_nombre ASC";
+            $consulta = mysqli_query($con->Conexion, $consultaCalles);
+
+            $filas = mysqli_num_rows($consulta);
+
+            if ($filas) $resultados = mysqli_fetch_all($consulta,  MYSQLI_ASSOC);
+
+            $con->CloseConexion();
+        }
+
+        $json_mensaje = json_encode($resultados);
+        echo $json_mensaje;        
+    }
 }
