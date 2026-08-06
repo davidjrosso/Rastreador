841280acbad784933d39e2ffcaa8a99914c66fcf
Monnittola <system.bridge.tec@gmail.com>
2026-08-06 20:47:30 -0300

    controlador de preferencias

---
+++ Controladores/PreferenciaController.php
@@ -0,0 +1,247 @@
+<?php
+/*
+*
+* This file is part of Rastreador3.
+*
+* Rastreador3 is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* Rastreador3 is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Rastreador3; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+require_once($_SERVER["DOCUMENT_ROOT"] . "/Controladores/Conexion.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . "/Modelo/Parametria.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . "/Modelo/Filtro.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . "/Modelo/Solicitud.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . "/Modelo/SolicitudItem.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . "/Modelo/TipoAccion.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . "/Modelo/TipoGrupoOperacion.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . "/Modelo/Account.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . "/Modelo/Accion.php");
+
+
+class PreferenciaController 
+{
+
+    public function listado_preferencias($mensaje = null)
+    {
+        header("Content-Type: text/html;charset=utf-8");
+        if (!isset($_SESSION["Usuario"])) {
+            include("../Error_Session.php");
+        } else {
+            $id_usuario = $_SESSION["Usuario"];
+            $account = new Account(account_id: $id_usuario);
+            $tipo_usuario = $account->get_id_tipo_usuario();
+            $Element = new Elements();
+            $DTGeneral = new CtrGeneral();
+
+            $mensaje_error = (isset($_REQUEST["MensajeError"])) ? $_REQUEST["MensajeError"] : "";
+            $mensaje_success = (isset($_REQUEST["Mensaje"])) ? $_REQUEST["Mensaje"] : "";
+
+            include("./Views/view_usuarios.php");
+        }
+        exit();
+    }
+
+    public function mod_preferencia(
+                                $id_account,
+                                $mensaje = null,
+                                $mensaje_error = null
+    ) {
+        header("Content-Type: text/html;charset=utf-8");
+        if (!isset($_SESSION["Usuario"])) {
+            include("../Error_Session.php");
+        } else {
+
+            $ID_Usuario = $_SESSION["Usuario"];
+            $usuario = new Account(account_id: $ID_Usuario);
+            $TipoUsuario = $usuario->get_id_tipo_usuario();
+
+            $AccountID = $_REQUEST["account_id"];
+            $usuario = new Account(account_id: $AccountID);
+            $lastname = ucfirst($usuario->get_last_name());
+            $firstname = ucwords($usuario->get_first_name());
+            $initials = strtoupper($usuario->get_initials());
+            $username = $usuario->get_user_name();
+            $userpass = $usuario->get_password();
+            $email = $usuario->get_email();
+            $ID_Tipo = $usuario->get_id_tipo_usuario();
+
+            $mensaje_error = (isset($_REQUEST["MensajeError"])) ? $_REQUEST["MensajeError"] : "";
+            $mensaje_success = (isset($_REQUEST["Mensaje"])) ? $_REQUEST["Mensaje"] : "";
+
+            include("./Views/view_modusuario.php");
+        }
+        exit();
+    }
+
+    public function datos_preferencia($id_preferencia)
+    {
+        header("Content-Type: text/html;charset=utf-8");
+        if (!isset($_SESSION["Usuario"])) {
+            include("../Error_Session.php");
+        } else {
+            include("../view_verusuarios.php");
+        }
+        exit();
+    }
+
+    public function dato_preferencia_user($id_account = null, $mensaje = null)
+    {
+        header("Content-Type: text/html;charset=utf-8");
+        if (!isset($_SESSION["Usuario"])) {
+            include("./Views/Error_Session.php");
+
+        } else {
+            $ID_Usuario = $_SESSION["Usuario"];
+            $usuario = new Account(account_id: $ID_Usuario);
+            $TipoUsuario = $usuario->get_id_tipo_usuario();
+
+            $AccountID = $_REQUEST["account_id"];
+            $account = new Account(account_id: $AccountID);
+            $exist_user = Account::exist_account(account_id: $AccountID);
+            $lastname = ucfirst($account->get_last_name());
+            $firstname = ucwords($account->get_first_name());
+            $initials = strtoupper($account->get_initials());
+            $username = $account->get_user_name();
+            $userpass = $account->get_password();
+            $email = $account->get_email();
+            $ID_Tipo = $account->get_id_tipo_usuario();
+
+            $mensaje_error = (isset($_REQUEST["MensajeError"])) ? $_REQUEST["MensajeError"] : "";
+            $mensaje_success = (isset($_REQUEST["Mensaje"])) ? $_REQUEST["Mensaje"] : "";
+
+            $Element = new Elements();
+
+            include("./Views/view_perfilusuario.php");
+        }
+
+        exit();
+    }
+
+    public function new_preferencia()
+    {
+        header("Content-Type: text/html;charset=utf-8");
+        if (!isset($_SESSION["Usuario"])) {
+            include("../Error_Session.php");
+        } else {
+            $id_usuario = $_SESSION["Usuario"];
+            $account = new Account(account_id: $id_usuario);
+            $tipo_usuario = $account->get_id_tipo_usuario();
+            $Element = new Elements();
+            $DTGeneral = new CtrGeneral();
+
+            $mensaje_error = (isset($_REQUEST["MensajeError"])) ? $_REQUEST["MensajeError"] : "";
+            $mensaje_success = (isset($_REQUEST["Mensaje"])) ? $_REQUEST["Mensaje"] : "";
+
+            include("./Views/view_newusuarios.php");
+        }
+         
+    }
+    public function mod_preferencia_control()
+    {
+
+        try {
+            
+        } catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        exit();
+    }
+
+    public function nueva_preferencia_control()
+    {
+        $id_solicitud = $_POST["id_solicitud"];
+        $fecha = date(format: "Y-m-d");
+        try {
+            header("Content-Type: text/html;charset=utf-8");
+            if (!isset($_SESSION["Usuario"])) {
+                include("../Error_Session.php");
+            } else {
+                $id_usuario = $_SESSION["Usuario"];
+                $account = new Account(account_id: $id_usuario);
+                $tipo_usuario = $account->get_id_tipo_usuario();
+                $con = new Conexion();
+                $con->OpenConexion();
+                $solicitud = new Solicitud(
+                                coneccion: $con,
+                                id_solicitud: $id_solicitud
+                    );
+                $filtro = new Filtro(
+                                coneccion_base: $con,
+                                id_usuario: $solicitud->get_id_usuario(),
+                                fecha: $fecha
+                    );
+                $filtro->save();
+
+                $con->CloseConexion();
+                $mensaje = "La solicitud fue registrada Correctamente";
+                header('Location: ../view_listados.php?Mensaje=' . $mensaje);
+            }
+        } catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    public function solicitud_preferencia_control()
+    {
+        $nombre_prefrencia = $_POST["id_usuario"];
+        $id_usuario =  $_POST["id_usuario"];
+        $lista_preferencia =  $_POST["lista_preferencias"];
+        $fecha = date(format: "Y-m-d");
+
+        try {
+            header("Content-Type: application/json;");
+            if (!isset($_SESSION["Usuario"])) {
+                include("../Error_Session.php");
+            } else {
+                $id_usuario = $_SESSION["Usuario"];
+                $account = new Account(account_id: $id_usuario);
+                $tipo_usuario = $account->get_id_tipo_usuario();
+                $con = new Conexion();
+                $con->OpenConexion();
+                $id_tipo_grupo_operacion = TipoGrupoOperacion::get_id_por_tipo(
+                                                    coneccion: $con,
+                                                    tipo: "PREFERENCIA"
+                                                    );
+                $id_tipo_accion = TipoAccion::get_id_tipo_acciones_por_tipo(
+                                                    coneccion: $con,
+                                                    tipo: "INSERT"
+                                                    );
+                $solicitud = new Solicitud(
+                                coneccion: $con,
+                                id_usuario: $id_usuario,
+                                fecha: $fecha,
+                                id_tipo_grupo_operacion: $id_tipo_grupo_operacion,
+                                id_tipo_accion: $id_tipo_accion
+                    );
+                $solicitud->save();
+                foreach($lista_preferencia as $key => $valor) {
+                    $solicitud_item = new SolicitudItem(
+                                coneccion: $con,
+                                id_solicitud: $solicitud->get_id_solicitud(),
+                                valor: $valor,
+                                identificador: $key
+                    );
+                    $solicitud_item->save();
+                }
+
+                $mensaje = "La solicitud fue registrada Correctamente";
+                
+            }
+            echo json_encode($mensaje);
+        } catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+}
