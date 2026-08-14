34ec3c162022c0c92614c2d0bf1b895c5145f92f
Monnittola <system.bridge.tec@gmail.com>
2026-08-14 15:20:25 -0300

    elementos de interfaz para operaciones en preferencias

--- Controladores/Elements.php
+++ Controladores/Elements.php
@@ -2901,13 +2901,33 @@ public function getMenuSeguridadUsuario($ID){
     $count = 0;
     while ($Ret = mysqli_fetch_array($obj_query)) {
       $div .= "<a  class='list-group-item list-group-item-action' data-toggle='list' role='tab' 
-                  id='list-" . $Ret['id_filtro'] . "-list' 
-                   
+                  id='list-" . $Ret['id_filtro'] . "-list'
                   href='#list" . $Ret['id_filtro'] . "' 
-                  
-                  aria-controls='" . $Ret['id_filtro'] . "'>" . 
-                        $Ret['titulo'] . 
-                "</a>";
+                  aria-controls='" . $Ret['id_filtro'] . "'
+                  style='display: flex; justify-content: space-between'>
+                      <span class='col-7'>" . $Ret['titulo'] . " </span>
+                  <div class='col-5' style='text-align: end;'>
+                    <button type='button' class='btn btn-outline-success' data-sel-filtro-id='" . $Ret['id_filtro'] . "'
+                            style='padding: 1px; border-radius: 43px;'>
+                      <svg xmlns='http://www.w3.org/2000/svg' width='26px' height='26px' viewBox='0 0 24 24'>
+                        <title xmlns=''>round-check</title>
+                          <path fill='currentColor' d='M9 16.17L5.53 12.7a.996.996 0 1 0-1.41 1.41l4.18 4.18c.39.39 1.02.39 1.41 0L20.29 7.71a.996.996 0 1 0-1.41-1.41z'/>
+                      </svg>
+                    </button>
+                    <button type='button' class='btn btn-outline-warning' data-mod-filtro-id='" . $Ret['id_filtro'] . "'
+                            style='padding: 1px; border-radius: 43px; padding-bottom: 5px;'>
+                      <svg xmlns='http://www.w3.org/2000/svg' width='26' height='18' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>
+                        <path d='M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z'/>
+                      </svg>
+                    </button>
+                    <button type='button' class='btn btn-outline-danger' data-del-filtro-id='" . $Ret['id_filtro'] . "'
+                            style='padding: 1px; border-radius: 43px;'>
+                      <svg xmlns='http://www.w3.org/2000/svg' width='26' height='26' fill='currentColor' class='bi bi-x-circle' viewBox='0 0 16 16'>
+                        <path d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708'/>
+                      </svg>
+                    </button>
+                  </div>
+               </a>";
       $count++;
     }
 
