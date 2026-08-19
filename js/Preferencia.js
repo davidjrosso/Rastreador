4982651ebfcb855c3188fc885ba30db1c75edff8
Monnittola <system.bridge.tec@gmail.com>
2026-08-19 08:16:39 -0300

    correccion de preferencias seleccion

--- js/Preferencia.js
+++ js/Preferencia.js
@@ -4,20 +4,23 @@ export class Preferencia {
 
     #listOpciones = [];    
 
-    newFiltro() {
+    newFiltro(char, num) {
         let val = $("#message-text").prop("value");
         if (!val) val = $("#text-filtro").prop("value");
-        let num = $("#list-tab-filtro a").length;
-        let char = String.fromCharCode(97 + num);
-        let node = $("<a class='list-group-item list-group-item-action " + ((num == 0) ? "active": "") + ` ' 
-                    id='list-` + char + `-list' 
+        $("#list-tab-filtro a").removeClass("active");
+        $("#list-tab-filtro a").removeClass("show");
+        $("#nav-tabContent-filtro a").removeClass("active");
+        $("#nav-tabContent-filtro a").removeClass("show");
+
+        let node = $("<a class='list-group-item list-group-item-action active show' " +
+                    `id='list-` + char + `-list' 
                     data-toggle='list' 
                     href='#list-` + char + `' 
                     role='tab' 
-                    aria-controls='` + char + `'>` + 
-                        val  + 
+                    aria-controls='` + char + `'>` +
+                        val  +
                 `</a>`);
-        let z = `  <div class='tab-pane fade ` + ((num == 0) ? "show active": "") + `'
+        let z = `  <div class='tab-pane fade show active'
                         id='list-` + char + `'
                         role='tabpanel'
                         aria-labelledby='list-` + char + `-list'>
@@ -579,11 +582,287 @@ export class Preferencia {
         }
     }
 
+    setDatos(id, value) {
+        switch (id) {
+            case "ID_Persona" :
+                nodo2.append(`<input type="text" name="id-persona" id = "id-persona" class="form-control" autocomplete="off">`);
+                $("#Persona").text(value.texto);
+                break;
+            case "Edad_Desde" :
+                nodo2.html("");
+                nodo1.prop("selectedIndex", index);
+                nodo1.prop("disabled", true);
+                nodo2.append(`<input type="text" name="Años - Desde" id = "Años - Desde" class="form-control" autocomplete="off">`);
+                nodo2.children().prop("value", value);
+                nodo2.children().prop("disabled", true);
+                this.#listOpciones.push(
+                                          {"Edad_Desde" : value,
+                                           "text" : texto}
+                                        );
+                break;
+            case "Edad_Hasta" :
+                nodo1.prop("selectedIndex", index);
+                nodo1.prop("disabled", true);
+                nodo2.html(`<input type="text" name="Años - Hasta" id = "Años - Hasta" class="form-control" autocomplete="off">`);
+                nodo2.children().prop("value", value);
+                nodo2.children().prop("disabled", true);
+                this.#listOpciones.push(
+                                          {"Edad_Hasta" : value,
+                                           "text" : texto}
+                                        );
+                break;
+            case "Meses_Desde" :
+                nodo1.prop("selectedIndex", index);
+                nodo1.prop("disabled", true);
+                nodo2.html(`<input type="text" name="Meses - Desde" id = "Meses - Desde" class="form-control" autocomplete="off">`);
+                nodo2.children().prop("value", value);
+                nodo2.children().prop("disabled", true);
+                this.#listOpciones.push(
+                                          {"Meses_Desde" : value,
+                                           "text" : texto}
+                                        );
+                break;
+            case "Meses_Hasta" :
+                nodo1.prop("selectedIndex", index);
+                nodo1.prop("disabled", true);
+                nodo2.html(`<input type="text" name="Meses - Hasta" id = "Meses - Hasta" class="form-control" autocomplete="off">`);
+                nodo2.children().prop("value", value);
+                nodo2.children().prop("disabled", true);
+                this.#listOpciones.push(
+                                        {"Meses_Hasta" : value,
+                                         "text" : texto}
+                                        );
+                break;
+            case "ID_Barrio" :
+                nodo1.prop("selectedIndex", 6);
+                nodo1.prop("disabled", true);
+                exis = this.#listOpciones.find(
+                                               (e) => e.hasOwnProperty("ID_Barrio")
+                                              );
+                op = $("#ID_Barrio").clone();
+                op.attr("id", "id-barrio-" +  num + "-" + e);
+                op.attr("name", "id-barrio-" +  num + "-" + e);
+                op.removeClass();
+                op.addClass("form-control");
+                op.prop("disabled", true);
+                if (!exis) this.#listOpciones.push({
+                                                      "ID_Barrio" : [],
+                                                      "text" : []
+                                                    }); 
+                let barrio = this.#listOpciones.find(
+                                        (e) => e.hasOwnProperty("ID_Barrio")
+                );
+
+                nodo2.html("");
+                nodo2.append(op);
+                op.prop("selectedIndex", value);
+                barrio["ID_Barrio"].push(valueOp);
+                barrio["text"].push(texto);
+                break;
+            case "Calle" :
+                nodo1.prop("selectedIndex", index);
+                nodo1.prop("disabled", true);
+                nodo2.html("");
+                nodo2.children().prop("value", value);
+                nodo2.children().prop("disabled", true);
+                this.#listOpciones.push(
+                                          {"Calle" : value,
+                                           "text" : texto}
+                                        );
+                break;
+            case "manzana" :
+                nodo1.prop("selectedIndex", index);
+                nodo1.prop("disabled", true);
+                nodo2.html(`<input type="text" name="Manzana" id = "Manzana" class="form-control" autocomplete="off">`);
+                nodo2.children().prop("value", value);
+                nodo2.children().prop("disabled", true);
+                this.#listOpciones.push({
+                                           "Manzana" : value,
+                                           "text" : texto
+                                         });
+                break;
+            case "lote" :
+                nodo1.prop("selectedIndex", index);
+                nodo1.prop("disabled", true);
+                nodo2.html(`<input type="text" name="Lote" id = "Lote" class="form-control" autocomplete="off">`);
+                nodo2.children().prop("value", value);
+                nodo2.children().prop("disabled", true);
+                this.#listOpciones.push({
+                                           "Lote" : value,
+                                           "text" : texto
+                                         });
+                break;
+            case "familia" :
+                nodo1.prop("selectedIndex", index);
+                nodo1.prop("disabled", true);
+                nodo2.html(`<input type="text" name="Sub-lote" id = "Sub-lote" class="form-control" autocomplete="off">`);
+                nodo2.children().prop("value", value);
+                nodo2.children().prop("disabled", true);
+                this.#listOpciones.push({
+                                           "Familia" : value,
+                                           "text" : texto
+                                         });
+                break;
+            case "ID_Categoria" :
+                nodo1.prop("selectedIndex", index);
+                nodo1.prop("disabled", true);
+                exis = this.#listOpciones.find(
+                                               (e) => e.hasOwnProperty("ID_Categoria")
+                                              );
+                nodo2.html(`<input type="text" name="categoria" id = "categoria" class="form-control" autocomplete="off">`);
+                nodo2.children().prop("disabled", true);
+                if (!exis) this.#listOpciones.push({
+                                                      "ID_Categoria" : [],
+                                                      "text" : []
+                                                    }); 
+                let categoria = this.#listOpciones.find(
+                                        (e) => e.hasOwnProperty("ID_Categoria")
+                );
+                categoria["ID_Categoria"].push(value);
+                if (!exis) texto = $("#Categoria").text();
+                if (exis) texto = $("#Categoria" + categoria["ID_Categoria"].length).text();
+                nodo2.children().prop("value", texto);
+                categoria["text"].push(texto);
+                break;
+            case "ID_Motivo" :
+                exis = this.#listOpciones.find(
+                                                   (e) => e.hasOwnProperty("ID_Motivo")
+                                                  );
+                nodo2.html("");
+                nodo2.children().prop("value", value);
+                nodo2.children().prop("disabled", true);
+                nodo2.html(`<input type="text" name="motivo" id = "motivo" class="form-control" autocomplete="off">`);
+                nodo2.children().prop("disabled", true);
+                if (!exis) this.#listOpciones.push(
+                                          {"ID_Motivo" : [],
+                                           "text" : []}
+                                             ); 
+                let motivo = this.#listOpciones.find(
+                                        (e) => e.hasOwnProperty("ID_Motivo")
+                );
+                motivo["ID_Motivo"].push(value);
+                if (!exis) texto = $("#Motivo").text();
+                if (exis) texto = $("#Motivo" + motivo["ID_Motivo"].length).text();
+                nodo2.children().prop("value", texto);
+                motivo["text"].push(texto);
+                break;
+            case "ID_Centro" :
+                nodo1.prop("selectedIndex", index);
+                nodo1.prop("disabled", true);
+                op = $("#ID_Centro").clone();
+                op.attr("id", "id-centro-" +  num + "-" + e);
+                op.attr("name", "id-centro-" +  num + "-" + e);
+                op.removeClass();
+                op.addClass("form-control");
+                op.prop("disabled", true);
+                nodo2.html("");
+                nodo2.append(op);
+                op.prop("selectedIndex", value);
+                this.#listOpciones.push(
+                                          {"ID_Centro" : value,
+                                           "text" : texto}
+                                        );
+                break;
+            case "Nro_Carpeta" :
+                nodo1.prop("selectedIndex", index);
+                nodo1.prop("disabled", true);
+                nodo2.html(`<input type="text" name="nro-carpeta" id = "nro-carpeta" class="form-control" autocomplete="off">`);
+                nodo2.children().prop("value", value);
+                nodo2.children().prop("disabled", true);
+                this.#listOpciones.push(
+                                          {"Nro_Carpeta" : value,
+                                           "text" : texto}
+                                        );
+                break;
+            case "Nro_Legajo" :
+                nodo1.prop("selectedIndex", index);
+                nodo1.prop("disabled", true);
+                nodo2.html(`<input type="text" name="nro-legajo" id = "nro-legajo" class="form-control" autocomplete="off">`);
+                nodo2.append();
+                nodo2.children().prop("value", value);
+                nodo2.children().prop("disabled", true);
+                this.#listOpciones.push(
+                                          {"Nro_Legajo" : value,
+                                           "text" : texto}
+                                        );
+                break;
+            case "ID_OtraInstitucion" :
+                nodo1.prop("selectedIndex", index);
+                nodo1.prop("disabled", true);
+                op = $("#ID_OtraInstitucion").clone();
+                op.attr("id", "id-ins-" +  num + "-" + e);
+                op.attr("name", "id-ins-" +  num + "-" + e);
+                op.prop("disabled", true);
+                nodo2.html("");
+                nodo2.append(op);
+                op.prop("selectedIndex", value);
+                this.#listOpciones.push(
+                                          {"ID_OtraInstitucion" : value,
+                                           "text" : texto}
+                                        );
+                break;
+            case "ID_Escuela" :
+                nodo1.prop("selectedIndex", index);
+                nodo1.prop("disabled", true);
+                op = $("#ID_Escuela").clone();
+                op.attr("id", "id-escuela-" +  num + "-" + e);
+                op.attr("name", "id-escuela-" +  num + "-" + e);
+                op.prop("disabled", true);
+                nodo2.html("");
+                nodo2.append(op);
+                op.prop("selectedIndex", value);
+                this.#listOpciones.push(
+                                          {"ID_Escuela" : value,
+                                           "text" : texto}
+                                        );
+                break;
+            case "ID_Responsable" :
+                nodo1.prop("selectedIndex", index);
+                nodo1.prop("disabled", true);
+                exis = this.#listOpciones.find(
+                                                   (e) => e.hasOwnProperty("ID_Responsable")
+                                                  );
+                op = $("#ID_Responsable").clone();
+                op.attr("id", "id-responsable-" +  num + "-" + e);
+                op.attr("name", "id-responsable-" +  num + "-" + e);
+                nodo2.html("");
+                nodo2.append(op);
+                op.prop("selectedIndex", value);
+                op.prop("disabled", true);
+                if (!exis) this.#listOpciones.push(
+                                          {"ID_Responsable" : [],
+                                           "text" : []}
+                                             ); 
+                let responsable = this.#listOpciones.find(
+                                        (e) => e.hasOwnProperty("ID_Responsable")
+                );
+                responsable["ID_Responsable"].push(valueOp);
+                responsable["text"].push(texto);
+                break;
+            case "inpMostrar" :
+                nodo1.prop("selectedIndex", index);
+                nodo1.prop("disabled", true);
+                op = $("#inpMostrar").clone();
+                op.attr("id", "id-mov-" +  num + "-" + e);
+                op.attr("name", "id-mov-" +  num + "-" + e);
+                op.prop("disabled", true);
+                nodo2.html("");
+                nodo2.append(op);
+                op.prop("value", value);
+                this.#listOpciones.push(
+                                          {"inpMostrar" : value,
+                                           "text" : texto}
+                                        );
+                break;
+        }
+    }
+
     datosFormulario() {
-        let num = $("#list-tab-filtro a").length - 1;
+        let num = $("#list-tab-filtro a").length;
         let char = String.fromCharCode(97 + num);
         let index = 1;
         let texto = "";
+        this.newFiltro(char, num);
         $("form input[data-pre]").each(function (ind, e) {
             if (!e.value || e.value == "0") return;
             this.addSelect(char, index);
@@ -603,6 +882,23 @@ export class Preferencia {
         this.addItemPreferencia( "titulo",
                                 $("#list-" + char + "-list").text().trim(),
                                 null);
+    }
+
+
+    setFormulario(respuesta) {
+        let num = $("#list-tab-filtro a").length - 1;
+        let char = String.fromCharCode(97 + num);
+        let index = 1;
+        let texto = "";
+        $("form input[data-pre]").each(function (ind, e) {
+            if (e.id in respuesta) return;
+            this.setDatos(e.id, respuesta.id);
+        }.bind(this));
+
+        $("form select[data-pre]").each(function (ind, e) {
+            if (e.id in respuesta) return;
+            this.setDatos(e.id, respuesta.id);
+        }.bind(this));
     }
 
     sendRequestModificarPreferencia() {
@@ -731,18 +1027,8 @@ export class Preferencia {
         contentType: "application/x-www-form-urlencoded",
         data: "id_solicitud=" + id,
         success : function (data, status, requestHttp) {
-            swal.fire({
-                title: "",
-                html: "Se envió al administrar la preferencia para su autorización.",
-                icon: "success",
-                customClass: {
-                    htmlContainer: "text-dialog"
-                },
-                showCloseButton: true,
-                showCancelButton: true,
-                confirmButtonText: `OK`,
-                cancelButtonText: `Cancel`
-            });
+
+            this.setDatos(data, null);
         }.bind(this),
         error: function (data, status, requestHttp) {
              swal.fire({
